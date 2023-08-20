05694bd97d91f492f41a24ad5111749a1905713a
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2023-08-20 00:45:25 -0400

    remove extra element

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,6 @@ import ProjectImage from "../assets/images/project.jpg"
 const Projects = () => {
   return (
     <div className="Projects">
-      <p>test</p>
       <div className="card-row">
         <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
         <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
