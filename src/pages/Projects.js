1f44fdc6eedc0a446dd24b0122ee1af2d7027e5d
HPel <elizabeth.lam099@gmail.com>
2023-08-31 16:26:00 -0400

    color scheme

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,11 +9,6 @@ const Projects = () => {
         <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
         <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
       </div>
-      <div className="card-row">
-        <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
-        <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
-        <Card image={ProjectImage} title="Awesome Project" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim neque volutpat ac tincidunt." link="https://hackthe6ix2023.devpost.com/" />
-      </div>
     </div>
   );
 };
