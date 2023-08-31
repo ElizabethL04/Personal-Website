43d8c1a9c25934808a71da5fb27ca71ba32e7ed5
Elizabeth Lam <elizabeth.lam099@gmail.com>
2023-08-31 14:52:40 -0400

    updates2.0

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,11 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div className='navbar'>
+      <Link className='navbar-link' to='/'>Home</Link>
+      <Link className='navbar-link' to='/projects'>Projects</Link>
+      <Link className='navbar-link' to='contact'>Contact</Link>
     </div>
   );
 };
